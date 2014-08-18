88a80618ef74deef0c544515c8569de17e66619d
xuesong.deng <dxuesong7@gmail.com>
2014-08-18 19:05:29 +0800

    Initial Commit

---
+++ GMM/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GMM
+//
+//  Created by deng on 14-8-18.
+//  Copyright (c) 2014年 deng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
