75ab960c02a60f71cd53f22c57cf4470cfa0120b
alanse7en <dxuesong7@gmail.com>
2014-08-26 16:02:52 +0800

    rewrite the comment to doxygen

--- GMM/GMM.h
+++ GMM/GMM.h
@@ -19,19 +19,19 @@ using namespace std;
 using namespace Eigen;
 
 struct fitOption {
-    /*
-     covType : 'diagonal' covariance matrices are restricted to be diagonal;
-               'spherical' covariance matrices are restricted to be spherical;
-               'full' covariance matrices are restricted to be full( default).
-     sharedCov : True if the component covariance matrices are restricted to be the same, default is false.
-     start : 'random' randomly select from train data to initialize model parameters;
-             'kmeans' use kmeans to get initial clustering index of the train data( default).
-     regularize : Regularization parameter to avoid covariance being singular, defalut is 0.
-     display : 'iter' show information during training (default); 'final' show final information.
-     maxIter : Maximum iterations allowed, default is 100;
-     tolFun : The termination tolerance for the log-likelihood function, default is 1e-6.
-     converged : 'true' GMM training process converged; otherwise failed.
-     iters : The num of iterations to finished training.
+    /*!
+     *covType : 'diagonal' covariance matrices are restricted to be diagonal;
+     *          'spherical' covariance matrices are restricted to be spherical;
+     *          'full' covariance matrices are restricted to be full( default).
+     *sharedCov : True if the component covariance matrices are restricted to be the same, default is false.
+     *start : 'random' randomly select from train data to initialize model parameters;
+     *        'kmeans' use kmeans to get initial clustering index of the train data( default).
+     *regularize : Regularization parameter to avoid covariance being singular, defalut is 0.
+     *display : 'iter' show information during training (default); 'final' show final information.
+     *maxIter : Maximum iterations allowed, default is 100;
+     *tolFun : The termination tolerance for the log-likelihood function, default is 1e-6.
+     *converged : 'true' GMM training process converged; otherwise failed.
+     *iters : The num of iterations to finished training.
      */
     string covType = "full";
     bool sharedCov = false;
@@ -45,13 +45,13 @@ struct fitOption {
 };
 
 class GMM {
-    /*
-     nComponents The number of mixture components, K;
-     nDimensions The number of dimensions for each Gaussian component, D;
-     mu A K-by-D matrix of component means;
-     Sigma A K-by-D-by-D matrix of component covariance matrices;
-     p A K-by-1 vector of the proportion of each component.
-     option Containing options about the GMM model.
+    /*!
+     *nComponents The number of mixture components, K;
+     *nDimensions The number of dimensions for each Gaussian component, D;
+     *mu A K-by-D matrix of component means;
+     *Sigma A K-by-D-by-D matrix of component covariance matrices;
+     *p A K-by-1 vector of the proportion of each component.
+     *option Containing options about the GMM model.
      */
     long nComponents;
     long nDimensions;
@@ -59,44 +59,44 @@ class GMM {
     vector<MatrixXd> Sigma;
     VectorXd p;
     fitOption option;
-    // Check the train data to ensure it is valid.
+    /// Check the train data to ensure it is valid.
     void checkData(MatrixXd data);
-    // Check the option to ensure it is valid.
+    /// Check the option to ensure it is valid.
     void checkOption(fitOption option);
-    // Check model to ensure it is valid.
+    /// Check model to ensure it is valid.
     void checkModel();
-    // Initialize model parameters.
+    /// Initialize model parameters.
     void initParam(MatrixXd data);
     
 public:
-    //  Constructors
+    ///  Constructors
     GMM(long nComponents, long nDimensions, fitOption option = fitOption());
     GMM(MatrixXd mu, vector<MatrixXd> Sigma, VectorXd p, fitOption option = fitOption());
-    //  Load GMM from fileName
+    ///  Load GMM from fileName
     GMM(string fileName);
-    //  Copy constructors
+    ///  Copy constructors
     GMM(GMM &gmm);
-    //  = operator
+    ///  = operator
     GMM& operator=(const GMM& gmm);
-    /*
-     Train GMM to fit data with option opt;
-     data A N-by-D matrix containing the train data.
+    /*!
+     *Train GMM to fit data with option opt;
+     *data A N-by-D matrix containing the train data.
      */
     void fit(MatrixXd data);
-    /*
-     data A N-by-D matrix containing the test data;
-     idx A N-by-1 vector representing N data points' correspoding Gaussian components;
-     nLogL The negative of the log-likelihood of the data;
-     post A N-by-K matrix containing the posterior peobability of p(component J | dataPoint I);
-     logPdf A N-by-1 vector containing estimates of the PDF of dataPoint I, the PDF of I is the
-     sum of p(dataPoint I | component J) * Pr(component J).
+    /*!
+     *data A N-by-D matrix containing the test data;
+     *idx A N-by-1 vector representing N data points' correspoding Gaussian components;
+     *nLogL The negative of the log-likelihood of the data;
+     *post A N-by-K matrix containing the posterior peobability of p(component J | dataPoint I);
+     *logPdf A N-by-1 vector containing estimates of the PDF of dataPoint I, the PDF of I is the
+     *sum of p(dataPoint I | component J) * Pr(component J).
      */
     void cluster(MatrixXd data, VectorXd &idx, int &nLogL, MatrixXd &post, VectorXd &logPdf);
-    //  Save GMM to fileName
+    ///  Save GMM to fileName
     void save(string fileName);
 };
 
-// Generate random permutation
+/// Generate random permutation
 int myRandom(int i);
 VectorXd randperm(unsigned long n);
 #endif /* defined(__GMM__GMM__) */
